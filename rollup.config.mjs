9b90b29f1df61e490160c03b0cf0b46b42a9daeb
kiochan <sunxingchen@live.com>
2022-12-08 02:11:53 +0100

    fix rollup configurations

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,19 +1,25 @@
 import typescript from "@rollup/plugin-typescript";
-import terser from '@rollup/plugin-terser';
 import dts from "rollup-plugin-dts";
+
+/*
+    use @wwa/rollup-plugin-terser instead of @rollup/plugin-terser
+    until this issue is resolved: https://github.com/rollup/plugins/issues/1366
+*/
+import { terser } from '@wwa/rollup-plugin-terser';
+
 
 export default [{
         input: 'src/index.ts',
         output: {
             sourcemap: true,
-            file: 'dest/test.js',
+            file: 'dist/index.mjs',
             format: 'es',
         },
         plugins: [typescript(), terser()],
     }, {
         input: 'src/index.ts',
         output: {
-            file: 'dest/test.d.ts',
+            file: 'types/index.d.ts',
             format: 'es',
         },
         plugins: [dts()],
