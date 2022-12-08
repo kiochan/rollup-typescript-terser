f37d18906903b444d47f483cbde24acaa204c93a
kiochan <sunxingchen@live.com>
2022-12-08 02:24:34 +0100

    add another file for testing of bundle

---
+++ src/add.ts
@@ -0,0 +1,9 @@
+/**
+ * add tow numbers
+ * @param n1 number
+ * @param n2 another number
+ * @return sum of n1 and n2
+ */
+export default function add (n1: number, n2: number): number {
+    return n1 + n2;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
+import add from "./add";
+
+/**
+ * Default class for export
+ */
 export default class ThisIsADefaultClass {
-    
+    static add = add;
 }
